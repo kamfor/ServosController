1cdd70fdf606fc0c34c48ad80c7738d892669728
kamfor <kfforex@gmail.com>
2016-04-11 15:22:55 +0200

    ardmini

---
+++ mini/mini.ino
@@ -0,0 +1,60 @@
+#include <Servo.h>
+
+Servo h;
+Servo v;  
+
+
+int hpos = 0;
+int vpos = 0; 
+
+void setup() {
+  h.attach(32);
+  v.attach(1); 
+  Serial.begin(9600);
+  while (!Serial) {
+  }
+  Serial.println('H'); 
+}
+
+void loop() {
+
+  
+
+  while (Serial.available() > 0) {
+    
+    int nvpos = Serial.parseInt();
+    
+    int nhpos = Serial.parseInt();
+    
+    nvpos = map(nvpos,0, 90, 25, 100);
+
+    nhpos = map(nhpos,0, 180, 10, 165);
+  
+    if (Serial.read() == '\n') { 
+      
+      while((!(vpos==nvpos))||(!(hpos==nhpos))){
+        
+        if((nvpos>vpos)&&(vpos!=nvpos)){
+          v.write(vpos);
+          vpos++; 
+        }
+        if((nvpos<vpos)&&(vpos!=nvpos)){
+          v.write(vpos);
+          vpos--; 
+        }
+         if((nhpos>hpos)&&(hpos!=nhpos)){
+          h.write(hpos);
+          hpos++; 
+        }
+        if((nhpos<hpos)&&(hpos!=nhpos)){
+          h.write(hpos);
+          hpos--; 
+        } 
+        delay(20); 
+      }
+
+      Serial.println("OK");
+    }
+  }
+}
+
